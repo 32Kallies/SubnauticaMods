464b38716a4bb55b1eadbba10d286817c766c7fc
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-04-27 02:20:12 -0400

    Fix water splash rendering in bases

--- WeatherMod/WeatherMod/Mono/WaterSplashVfxController.cs
+++ WeatherMod/WeatherMod/Mono/WaterSplashVfxController.cs
@@ -5,11 +5,31 @@ namespace WeatherMod.Mono;
 public class WaterSplashVfxController : MonoBehaviour
 {
     public Transform affectedTransform;
+    private ParticleSystem _particles;
+    private bool _hasParticles;
     
     public float heightOffset = 0.1f;
-    
+
+    private void Start()
+    {
+        _particles = affectedTransform.GetComponent<ParticleSystem>();
+        _hasParticles = _particles != null;
+        if (!_hasParticles)
+        {
+            Plugin.Logger.LogWarning("Particle system not found for WaterSplashVFX");
+        }
+    }
+
     private void LateUpdate()
     {
+        if (_hasParticles)
+        {
+            var indoors = Player.main.IsInside();
+            _particles.SetPlaying(!indoors);
+            if (indoors)
+                return;
+        }
+        
         var camPos = MainCamera.camera.transform.position;
         affectedTransform.position = new Vector3(camPos.x, Ocean.GetOceanLevel() + heightOffset, camPos.z);
     }
