315677086e3f64093a7e187922c3da31d1726196
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-04-27 02:22:03 -0400

    Adjust durations of various weather events

--- WeatherMod/WeatherMod/WeatherEvents/ClearSkies.cs
+++ WeatherMod/WeatherMod/WeatherEvents/ClearSkies.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using WeatherMod.Mono;
 
 namespace WeatherMod.WeatherEvents;
 
@@ -8,8 +7,8 @@ public class ClearSkies : WeatherEvent
     protected override GameObject EffectPrefab { get; } = null;
     protected override float DestroyDelay { get; } = 0;
     protected override FogSettings Fog { get; } = new FogSettings();
-    public override float MinDuration { get; } = 45;
-    public override float MaxDuration { get; } = 120;
+    public override float MinDuration { get; } = 3 * 60;
+    public override float MaxDuration { get; } = 9 * 60;
     public override WeatherEventAudio AmbientSound { get; } = null;
 
     protected override void OnEventBegin(GameObject effectPrefab) { }
--- WeatherMod/WeatherMod/WeatherEvents/LightRain.cs
+++ WeatherMod/WeatherMod/WeatherEvents/LightRain.cs
@@ -8,8 +8,8 @@ public class LightRain : WeatherEvent
     protected override GameObject EffectPrefab { get; } = Plugin.AssetBundle.LoadAsset<GameObject>("Weather_LightRain");
     protected override float DestroyDelay { get; } = 5;
     protected override FogSettings Fog { get; } = new FogSettings(0.0014f, new Color(0.18f, 0.26f, 0.45f), 0.5f, 0.54f, 0.7f);
-    public override float MinDuration { get; } = 60;
-    public override float MaxDuration { get; } = 120;
+    public override float MinDuration { get; } = 4 * 60;
+    public override float MaxDuration { get; } = 6 * 60;
     public override WeatherEventAudio AmbientSound { get; } = new WeatherEventAudio(WeatherAudio.LightRainLoop, WeatherAudio.LightRainInsideLoop, null);
     public override int RainDropVfxEmission { get; } = 20;
 
